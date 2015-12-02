a01faf354960b6c25a690cba41b958bee0644755
Yonas Kbrom <kbromyonas@berkeley.edu>
2015-12-02 07:14:58 +0000

    new stuff

--- proj2/db/seeds.rb
+++ proj2/db/seeds.rb
@@ -10,17 +10,48 @@ colleges = [ "Berkeley", "Stanford", "Community College" ]
 colleges.each do |name|
 	College.create name: name
 end
-subjects = [ ["social sciences", 1], ["biological sciences", 1], ["mathematics", 1], ["engineering sciences", 1], ["arts", 1] ]
+subjects = [ ["Social Sciences", 1], ["Biological Sciences", 1], ["Mathematics", 1], ["Engineering Sciences", 1], ["Arts", 1] ]
 subjects.each do |name, college_id|
 	Subject.create name: name, college_id: college_id
-	Subject.create name: name, college_id: college_id + 1
-	Subject.create name: name, college_id: college_id + 2
 end
-courses = [ ["English 16A", 1, "Introduction to Creative Writing."], ["English 16B", 1, "Advnaced composiiton"], ["MCB 9000", 2, "Advanced af Molecular and Cellular Biology"], ["Bio 12", 2, "super advanced bio."], ["Math 1", 3, "Basic Calculus"], ["Engineering 1", 4, "basic engineering."], ["Dancing 1", 5, "Learn to dance."], ["Dancing 2", 5, "asdf."] ]
+subjects = [ ["Social Sciences", 2], ["Biological Sciences", 2], ["Mathematics", 2], ["Engineering Sciences", 2], ["Arts", 2] ]
+subjects.each do |name, college_id|
+	Subject.create name: name, college_id: college_id
+end
+subjects = [ ["Social Sciences", 3], ["Biological Sciences", 3], ["Mathematics", 3], ["Engineering Sciences", 3], ["Arts", 3] ]
+subjects.each do |name, college_id|
+	Subject.create name: name, college_id: college_id
+end
+courses = [ ["English 16A", 1, "Introduction to Creative Writing."], 
+  ["English 16B", 1, "Advanced composiiton"], 
+  ["MCB 9000", 2, "Advanced af Molecular and Cellular Biology"], 
+  ["Bio 12", 2, "Advanced Biology"], 
+  ["Math 1", 3, "Basic Calculus"], 
+  ["Engineering 1", 4, "basic engineering."], 
+  ["Dancing 1", 5, "Learn to dance."], 
+  ["Dancing 2", 5, "asdf."],
+  
+  ["English 1A", 6, "Intro to ABC's."], 
+  ["English 1B", 6, "2nd Grade Reading"], 
+  ["Physics 9000", 7, "Pretty much elementary physics."], 
+  ["Chem 12", 7, "Advanced Chemistry, not really though."], 
+  ["Math 20A", 8, "Basic Addition and Subtraction"], 
+  ["Engineering 10", 9, "Introduction to Legos."], 
+  ["Art 10A", 10, "Finger Painting."], 
+  ["Media Studies 20", 10, "Introduction to Sesame Street"],
+  
+  ["Literature 32A", 11, "Introduction to Creative Writing."], 
+  ["Literature 32B", 11, "Advanced composiiton"], 
+  ["MCB 93", 12, "Advanced Molecular and Cellular Biology"], 
+  ["Biology 64", 12, "Advanced Biology"], 
+  ["Math 104", 13, "Basic Calculus"], 
+  ["EE 16A", 14, "basic engineering."], 
+  ["Rap 6", 15, "Learn to dance."], 
+  ["Country 10", 15, "asdf."] ]
 courses.each do |name, subject, description|
 	Course.create name: name, subject_id: subject, description: description
 end
-users = [ ["asdf@asdf.com", "asdfasdf", "asdf", "comp sci", "freshman", "Cal" ], ["tim@email.com", "asdfasdf", "tim", "Math", "junior", "Cal"] ]
+users = [ ["johndoe@gmail.com", "youknowit", "John", "Philosophy", "Freshman", "Cal" ], ["timotheyc@email.com", "menameistimmy", "Tim Caudillo", "Math", "Junior", "UCLA"] ["bryantkobe@email.com", "GOAT", "Kobe Bryant", "Cognitive Science", "Sophmore", "USC"] ]
 users.each do |email, pass, name, major, year, school|
 	user = User.new
 	user.email = email
@@ -31,7 +62,13 @@ users.each do |email, pass, name, major, year, school|
 	user.school = school
 	user.save!
 end
-posts = [ ["Math is hard. Also dumb.", 1, "Math is terrible.", 1], ["Math is hard. Also dumb.", 1, "Math is terrible.", 1], ["Math is hard. Also dumb.", 1, "Math is terrible.", 1], ["Science cool.", 2, "Science post.", 2], ["Dancing", 3, "Dance post.", 2], ["Dancing is fun", 3, "I like dancing.", 1], ["I hate biology", 6, "Biology is the worst thing to ever exist.", 1] ]
+posts = [ ["Great Class.", 1, "", 1], 
+  ["Engineering is my life.", 6, "This class gave me hives.", 3], 
+  ["Basic calculus", 5, "The prof was pretty much not understandable. I got an F.", 3], 
+  ["Science cool.", 3, "Science post.", 2], 
+  ["Dancing", 8, "Dance post.", 2], 
+  ["Dancing is fun", 7, "I like dancing.", 1], 
+  ["I hate biology", 4, "Biology is the worst thing to ever exist.", 1] ]
 posts.each do |title, course_id, content, user_id|
 	Post.create title: title, course_id: course_id, content: content, user_id: user_id
 end

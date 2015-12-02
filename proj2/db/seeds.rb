e1061f4bbd34cd0606e28f893020d129f67603e6
Daniel Leahy <daniel_leahy@berkeley.edu>
2015-12-01 23:25:52 -0800

    fuby

--- proj2/db/seeds.rb
+++ proj2/db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-colleges = [ "Berkeley", "Stanford", "Community College" ]
+colleges = [ "Berkeley", "Stanford", "USC" ]
 colleges.each do |name|
 	College.create name: name
 end
@@ -37,6 +37,7 @@ courses = [ ["English 16A", 1, "Introduction to Creative Writing."],
   ["Chem 12", 7, "Advanced Chemistry, not really though."], 
   ["Math 20A", 8, "Basic Addition and Subtraction"], 
   ["Engineering 10", 9, "Introduction to Legos."], 
+  ["CS 55", 9, "Supervised iPad Free Time"]
   ["Art 10A", 10, "Finger Painting."], 
   ["Media Studies 20", 10, "Introduction to Sesame Street"],
   
@@ -46,8 +47,8 @@ courses = [ ["English 16A", 1, "Introduction to Creative Writing."],
   ["Biology 64", 12, "Advanced Biology"], 
   ["Math 104", 13, "Basic Calculus"], 
   ["EE 16A", 14, "basic engineering."], 
-  ["Rap 6", 15, "Learn to dance."], 
-  ["Country 10", 15, "asdf."] ]
+  ["Rap 6", 15, "Learn to Spit Bars."], 
+  ["Music 10", 15, "17th Century Symphonies."] ]
 courses.each do |name, subject, description|
 	Course.create name: name, subject_id: subject, description: description
 end
@@ -62,13 +63,36 @@ users.each do |email, pass, name, major, year, school|
 	user.school = school
 	user.save!
 end
-posts = [ ["Great Class.", 1, "", 1], 
-  ["Engineering is my life.", 6, "This class gave me hives.", 3], 
-  ["Basic calculus", 5, "The prof was pretty much not understandable. I got an F.", 3], 
-  ["Science cool.", 3, "Science post.", 2], 
-  ["Dancing", 8, "Dance post.", 2], 
+posts = [ ["Great Class.", 1, "Love this class. Easy A. Highly recommend", 1], 
+  ["Engineering is my life.", 6, "This class gave me hives.", 1], 
+  ["Basic calculus", 5, "The prof was pretty much not understandable. I got an F.", 1], 
+  ["Science cool.", 3, "Science post.", 1], 
+  ["Dancing", 8, "Dance post.", 1], 
   ["Dancing is fun", 7, "I like dancing.", 1], 
-  ["I hate biology", 4, "Biology is the worst thing to ever exist.", 1] ]
+  ["I hate biology", 4, "Biology is the worst thing to ever exist.", 1] 
+  
+  ["Hard Clas.", 9, "reely hard. Took me three weecs to get passd d", 2], 
+  ["I kept stepping on them.", 14, "Make sure you wear shoes.", 2], 
+  ["Too hard", 13, "I have no idea what was going on, failed the final.", 2], 
+  ["Science cool.", 11, "Its nise.", 2], 
+  ["Dancing", 16, "Dance post.", 2], 
+  ["Dancing is fun", 15, "I like dancing.", 2], 
+  ["Is this the Krusty Krab", 12, "No this is Patrick.", 2]
+  
+  ["Good English Class.", 18, "Teacher was clear, and assignments were reasonable. Recommend", 1], 
+  ["Engineering is my life.", 23, "This class gave me hives.", 3], 
+  ["Basic calculus", 22, "The prof was pretty much not understandable. I got an F.", 3], 
+  ["Science cool.", 20, "Science post.", 2], 
+  ["Dancing", 25, "Great dance class. Learned lots of new moves.", 2], 
+  ["Dancing is fun", 24, "I like dancing.", 1], 
+  ["I hate biology", 21, "Biology is the worst thing to ever exist.", 1]
+  
+  ]
 posts.each do |title, course_id, content, user_id|
 	Post.create title: title, course_id: course_id, content: content, user_id: user_id
 end
+
+
+
+
+
